20b488d5b5d8f74176abc959390c0d0445889eb7
miraoto <miraoto@gmail.com>
2015-11-16 10:15:48 +0900

    とりあえず動くようにした

--- lib/ruboty/feedly/actions/feedly.rb
+++ lib/ruboty/feedly/actions/feedly.rb
@@ -1,3 +1,5 @@
+require 'feedlr'
+
 module Ruboty
   module Feedly
     module Actions
@@ -8,8 +10,27 @@ module Ruboty
 
         private
 
+        # rate limiting of 250 API calls a day(https://developer.feedly.com/v3/#rate-limiting
+        # API access token expired 7 days
         def news_feed
-          'none'
+          begin
+            client = Feedlr::Client.new(oauth_access_token: ENV['FEEDLY_OAUTH_ACCESS_TOKEN'])
+            @latest_list = {}
+            client.user_unread_counts.unreadcounts.each do |unread_articles|
+              next if unread_articles['count'] == 0
+              unread_article_items = client.stream_entries_contents(unread_articles.id, unreadOnly: true).items
+              next if unread_article_items.empty?
+
+              unread_article_items.each do |article|
+                @latest_list[article.alternate[0].href] = "◼︎ <a href='#{article.alternate[0].href}'>#{article.title} - #{article.origin.title}</a>"
+              end
+              client.mark_article_as_read(unread_articles.id)
+            end
+            @latest_news = @latest_list.values.join("<br>")
+          rescue => evar
+            fail evar
+          end
+          @latest_news
         end
       end
     end
